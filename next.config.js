a087919b3dd1f49b65d9caadd7db0df378284c78
unknown <saad123mn123@gmail.com>
2023-09-13 00:07:16 +0200

    export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    target: "server",
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
 }
 
 module.exports = nextConfig
