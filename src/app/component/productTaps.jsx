85c36a77433a31069d3ee2892034b3556a7c51bc
unknown <saad123mn123@gmail.com>
2023-09-14 01:40:21 +0200

    product-page 50%

---
+++ src/app/component/productTaps.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Tap = ({title , openTab , setOpenTab , i})=>{
+    return(
+        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                  (openTab === i
+                    ? "text-white bg-red-600"
+                    : "text-red-600 bg-white  hover:opacity-50 ")
+                }
+                onClick={e => {
+                  e.preventDefault();
+                  setOpenTab(i);
+                }}
+                data-toggle="tab"
+                href="#link2"
+                role="tablist"
+              >
+                 {title}
+              </a>
+            </li>
+    )
+}
+const TapContent = ({content , openTab , i , id , image,intro}) => (
+    <div className={openTab === i ? "block w-full" : "hidden"} id={id}>
+        <div className="content">
+        <div className="header text-2xl sm:text-4xl font-semibold py-5">{intro}</div>
+      <div>
+      <div className="justify-center py-8 w-full">
+       <div className="wrapper flex justify-center">
+       {Array.isArray(image) ? (
+          image.map((e, index) => <div className="inner-wrapper"><img key={index} src={e} alt="details" className="w-full h-full"/></div>)
+        ) : (
+          null
+        )}
+       </div>
+        <div className="content-p pt-10">
+        {content}
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+
+
+export {TapContent , Tap } ;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,11 +1,132 @@
+
 "use client"
+import { useState } from "react";
+import {Tap , TapContent} from "../component/productTaps"
+
+const companiesLevelOne = [
+  {
+    title: "Rockwell Automation",
+    intro: <h1>Industrial Automation Hardware & Software</h1>,
+    content: 
+      <>
+      <p>El-Sewedy Automation is recognized by its customers as the best in the market for using Allen-Bradley® and Rockwell Software® products since El-Sewedy Automation founders have a huge significant experience of using Rockwell Automation solutions in different Egyptian industrial markets.</p>
+      <p>Rockwell Automation Products:</p>
+      <ul>
+        <li>Programmable Controllers (PLC and PAC)</li>
+        <li>Input/Output (I/O) Modules</li>
+        <li>Networks Security & Infrastructure</li>
+        <li>Condition Monitoring</li>
+        <li>Motion Control</li>
+        <li>Drives & Motors</li>
+        <li>Power Supplies</li>
+        <li>Rockwell Software (SCADA)</li>
+        <li>Motor Control Contactors</li>
+        <li>Connection Devices</li>
+        <li>Circuit & Load Protection</li>
+        <li>Lighting Control</li>
+        <li>Sensors & Switches</li>
+        <li>Operator Interfaces (HMI)</li>
+        <li>Push Buttons & Signaling Devices</li>
+        <li>Relays & Timers</li>
+        <li>Safety Products</li>
+        <li>Signal Interface</li>
+        <li>Energy Monitoring</li>
+      </ul>
+      </>
+    ,
+    image: ["/images/details/rockwell.jpg","/images/details/Rockwel.jpg"],
+  },
+  {
+    title: "Siemens Automation",
+    intro: <h1>Industrial Automation Hardware & Software</h1>,
+    content: <>
+            <p>SIMATIC Controllers</p>
+              <ul>
+                <li>Distributed I/O Systems</li>
+                <li>Industrial Controls</li>
+                <li>SIMATIC HMI</li>
+                <li>Field Process Instrumentation</li>
+                <li>Automation & SCADA Software</li>
+                <li>Drive Technology (Low voltage Converters)</li>
+                <li>Drive Technology (Servo Converters)</li>
+              </ul>
+            </>,
+    image: ["/images/details/siemens.jpg"],
+  },
+  {
+    title: "GHM Group",
+    intro: <h1>Industrial Measurement Electronics</h1>,
+    content: <>
+            <p>As a distributor to GHM Group, GHM is a specialist and full-range supplier for measurement, industrial electronics, environmental measurement, microclimate-analysis, acoustic, light metering, and water-analysis. GHM Messtechnik products for Industrial automation can be classified as follows:</p>
+              <ul>
+                <li>Sensors (Level, Flow, Pressure ...etc.)</li>
+                <li>Industrial Electronics</li>
+                <li>Handheld Gauges</li>
+                <li>Sil/Atex Certified</li>
+                <li>Fittings</li>
+                <li>Data Logger</li>
+                <li>Electrical Connection Technology</li>
+                <li>Software & Programming Devices & Accessories</li>
+              </ul>
+            </>,
+    image: ["/images/details/ghm.jpg"],
+  },
+  {
+    title: "Deutschmann Automation",
+    intro: <h1>Industrial Ethernet, Gateways, and Network Security Solutions</h1>,
+    content: <>
+      <p>As an exclusive agent to DA, a German company founded exactly 40 years ago, the company has designed and manufactured innovative network components for industrial data communication:</p>
+      <ul>
+        <li>Protocol Converter</li>
+        <li>Embedded Solutions</li>
+        <li>Gateways</li>
+        <li>Electronic cam controls</li>
+        <li>Starter Kits & Accessories</li>
+      </ul>
+              </>,
+    image: "deutschmann-automation.jpg",
+  },
+];
+
+// You can now access each company's information by indexing the 'companies' array.
 
 export default function Products() {
+  const [openTab, setOpenTab] = useState(1);
+  
   return (
     <>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-center text-4xl">Products</h1>
+    <main className="flex min-h-screen justify-center">
+    <div className="w-11/12 mx-auto">
+        <div className="w-full">
+          <ul
+            className="flex mb-0 list-none sm:flex-wrap pt-3 pb-4 flex-row overflow-scroll"
+            role="tablist"
+          >
+            <Tap title={'Rockwell Automation'} openTab={openTab} setOpenTab={setOpenTab} i={1}/>
+            <Tap title={'Siemens Automation'} openTab={openTab} setOpenTab={setOpenTab} i={2}/>
+            <Tap title={'GHM Group'} openTab={openTab} setOpenTab={setOpenTab} i={3}/>
+            <Tap title={'Deutschmann Automation'} openTab={openTab} setOpenTab={setOpenTab} i={4}/>
+          </ul>
+        </div>
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+            <div className="px-4 py-5 flex-auto">
+              <div className="tab-content tab-space">
+              {companiesLevelOne.map((info , i)=>(
+                  <TapContent 
+                      content={info.content}
+                      openTab={openTab}
+                      intro={info.intro}
+                      i={i+1}
+                      id={`'link'${i}`}
+                      image={info.image}
+                      />
+                ))}
+              </div>
+            </div>
+          </div>
+      </div>
     </main>
     </>
   )
 }
+
