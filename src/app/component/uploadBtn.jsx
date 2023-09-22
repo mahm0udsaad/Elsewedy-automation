3e5a4697ad371ae49b62ac41328a34ab5d00d385
unknown <saad123mn123@gmail.com>
2023-09-23 00:59:25 +0300

    100

--- src/app/component/uploadBtn.jsx
+++ src/app/component/uploadBtn.jsx
@@ -5,7 +5,7 @@ const UploadBtn = () => {
     <div className="flex items-center justify-center w-full">
       <label
         htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-200   hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 hover:bg-gray-200"
+        className="flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-200   hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 hover:bg-gray-200"
       >
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
