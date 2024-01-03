bae6819f0bb094191b71ca2987d8afe90d49e6ea
unknown <saad123mn123@gmail.com>
2024-01-03 18:23:53 +0200

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'http://www.elsewedy-automation.com/',
+    siteUrl: process.env.SITE_URL || 'https://www.elsewedy-automation.com/',
     generateRobotsTxt: true,
   }
